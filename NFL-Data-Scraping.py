08d954f159318ea86d0cd4d683e157250b20aad6
tejasnaik <naik.tejas11@gmail.com>
2025-09-21 22:06:54 -0400

    first commit

---
+++ NFL-Data-Scraping.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from datetime import datetime
+
+current_year = datetime.now().year
+
+current_year -= 1
+
+url = f"https://www.pro-football-reference.com/years/{current_year}/games.htm"

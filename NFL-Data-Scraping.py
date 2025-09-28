fc3b1457bdce75bd6284acf01533f59a11057b48
tejasnaik <naik.tejas11@gmail.com>
2025-09-28 16:58:23 -0400

    checking the location and renaming it

--- NFL-Data-Scraping.py
+++ NFL-Data-Scraping.py
@@ -10,8 +10,17 @@ url = f"https://www.pro-football-reference.com/years/{current_year}/games.htm"
 # Read all tables from the page
 tables = pd.read_html(url)
 
-# Main schedule table is usually the first one
+# Main schedule table
 schedule = tables[0]
 
-# Drop repeated header rows
+# Droping the repeated header rows
 schedule = schedule[schedule['Week'] != 'Week']
+
+# The home/away might be named 'Unnamed: 5'
+home_away_col = 'Unnamed: 5' if 'Unnamed: 5' in schedule.columns else 'Location'
+
+# Keeping the columns we care about
+schedule = schedule[['Winner/tie', 'Loser/tie', 'PtsW', 'PtsL', home_away_col]]
+
+# Renaming the columns
+schedule.columns = ['Team', 'Opponent', 'Team_pts', 'Opponent_pts', 'Home/Away']

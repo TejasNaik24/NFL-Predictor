92c54969d616763c1ad861f08d56fc8d6407e410
tejasnaik <naik.tejas11@gmail.com>
2025-09-28 01:52:05 -0400

    added dropping week

--- NFL-Data-Scraping.py
+++ NFL-Data-Scraping.py
@@ -7,6 +7,11 @@ current_year -= 1
 
 url = f"https://www.pro-football-reference.com/years/{current_year}/games.htm"
 
+# Read all tables from the page
 tables = pd.read_html(url)
 
+# Main schedule table is usually the first one
 schedule = tables[0]
+
+# Drop repeated header rows
+schedule = schedule[schedule['Week'] != 'Week']

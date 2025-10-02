0cff267ec7151737f120bc3a390ff8d709df7117
tejasnaik <naik.tejas11@gmail.com>
2025-10-02 00:39:04 -0400

    added predictor file

---
+++ Predictor.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline

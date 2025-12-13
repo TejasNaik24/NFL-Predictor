03fa5b5deb651710ad44ab2ff8101a1ce039c6c1
tejasnaik <naik.tejas11@gmail.com>
2025-12-13 01:28:50 -0500

    intial UI finished

--- app.py
+++ app.py
@@ -9,16 +9,27 @@ st.set_page_config(
 )
 
 # ---- Centered Title ----
-col1, col2, col3 = st.columns([1, 2, 0.2])
+col1, col2, col3 = st.columns([1, 2, 0.5])
 with col2:
     st.title("NFL Predictor")
 
-# ---- Centered Image ----
+# Session state
+if "clicked" not in st.session_state:
+    st.session_state.clicked = False
+
+image_path = os.path.join(os.getcwd(), "static", "nfl.png")
+
+# ---- Centered Image + Button container ----
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
-    st.image("static/nfl.png", width=220)
+    image_placeholder = st.empty()
 
-# ---- Centered Button ----
-col1, col2, col3 = st.columns([1, 0.3, 1])
-with col2:
-    st.button("Predict")
+    if not st.session_state.clicked:
+        image_placeholder.image(image_path, caption="NFL Logo")
+
+        col_l, col_c, col_r = st.columns([1, 2, 1])
+        if col_c.button("Predict", key="predict_button"):
+            st.session_state.clicked = True
+            st.rerun()
+    else:
+        image_placeholder.empty()

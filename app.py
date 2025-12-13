b0c3efa4f960bb8471ecd32dcdd7c5f823f20469
tejasnaik <naik.tejas11@gmail.com>
2025-12-13 01:15:07 -0500

    started web dev

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 import streamlit as st
+import os
+
+icon_path = os.path.join("static", "nfl.png")
+
+st.set_page_config(
+    page_title="NFL Predictor",
+    page_icon=icon_path,
+)
+
+# ---- Centered Title ----
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.title("NFL Predictor")
+
+# ---- Centered Image ----
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+    st.image("static/nfl.png", width=220)
+
+# ---- Centered Button ----
+col1, col2, col3 = st.columns([1, 0.5, 1])
+with col2:
+    st.button("Predictor")

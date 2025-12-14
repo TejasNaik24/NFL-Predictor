b3ba42ce0261eeaf87b7b86f4da38ab6e1b96834
tejasnaik <naik.tejas11@gmail.com>
2025-12-13 21:41:38 -0500

    changed UI controls view

--- app.py
+++ app.py
@@ -16,8 +16,11 @@ if "clicked" not in st.session_state:
     st.session_state.clicked = False
 if "flow" not in st.session_state:
     st.session_state.flow = None  # None | "predicting" | "training"
+if "control_mode" not in st.session_state:
+    st.session_state.control_mode = "Choose Team"  # Default to Choose Team so dropdowns are enabled
 
 # ---- Centered Title (ALWAYS VISIBLE) ----
+st.markdown("")  # Minimal space at top
 col1, col2, col3 = st.columns([1, 1, 0.5])
 with col2:
     st.title("NFL Predictor")
@@ -105,20 +108,23 @@ if st.session_state.flow == "predicting":
     # Top bracket row
     cols = st.columns([1, 1, 1, 0.9, 1, 1, 1])
 
+    # Determine if dropdowns should be disabled
+    dropdowns_disabled = (st.session_state.control_mode == "AutoML")
+
     # ---- AFC Wild / Bye ----
     with cols[0]:
         st.markdown("**AFC Wild**")
-        st.selectbox("AFC Bye", afc_teams, key="afc_bye", label_visibility="collapsed")
+        st.selectbox("AFC Bye", afc_teams, key="afc_bye", label_visibility="collapsed", disabled=dropdowns_disabled)
         st.markdown("Bye Week")
         vspace(1) 
-        st.selectbox("Wild Card 1", afc_teams, key="afc_wild1", label_visibility="collapsed")
-        st.selectbox("Wild Card 2", afc_teams, key="afc_wild2", label_visibility="collapsed")
+        st.selectbox("Wild Card 1", afc_teams, key="afc_wild1", label_visibility="collapsed", disabled=dropdowns_disabled)
+        st.selectbox("Wild Card 2", afc_teams, key="afc_wild2", label_visibility="collapsed", disabled=dropdowns_disabled)
         vspace(1)
-        st.selectbox("Wild Card 3", afc_teams, key="afc_wild3", label_visibility="collapsed")
-        st.selectbox("Wild Card 4", afc_teams, key="afc_wild4", label_visibility="collapsed")
+        st.selectbox("Wild Card 3", afc_teams, key="afc_wild3", label_visibility="collapsed", disabled=dropdowns_disabled)
+        st.selectbox("Wild Card 4", afc_teams, key="afc_wild4", label_visibility="collapsed", disabled=dropdowns_disabled)
         vspace(1)
-        st.selectbox("Wild Card 5", afc_teams, key="afc_wild5", label_visibility="collapsed")
-        st.selectbox("Wild Card 6", afc_teams, key="afc_wild6", label_visibility="collapsed")
+        st.selectbox("Wild Card 5", afc_teams, key="afc_wild5", label_visibility="collapsed", disabled=dropdowns_disabled)
+        st.selectbox("Wild Card 6", afc_teams, key="afc_wild6", label_visibility="collapsed", disabled=dropdowns_disabled)
 
     # ---- AFC Divisional ----
     with cols[1]:
@@ -164,35 +170,54 @@ if st.session_state.flow == "predicting":
     # ---- NFC Wild / Bye ----
     with cols[6]:
         st.markdown("**NFC Wild**")
-        st.selectbox("NFC Bye", nfc_teams, key="nfc_bye", label_visibility="collapsed")
+        st.selectbox("NFC Bye", nfc_teams, key="nfc_bye", label_visibility="collapsed", disabled=dropdowns_disabled)
         st.markdown("Bye Week")
         vspace(1)
-        st.selectbox("Wild Card 1", nfc_teams, key="nfc_wild1", label_visibility="collapsed")
-        st.selectbox("Wild Card 2", nfc_teams, key="nfc_wild2", label_visibility="collapsed")
+        st.selectbox("Wild Card 1", nfc_teams, key="nfc_wild1", label_visibility="collapsed", disabled=dropdowns_disabled)
+        st.selectbox("Wild Card 2", nfc_teams, key="nfc_wild2", label_visibility="collapsed", disabled=dropdowns_disabled)
         vspace(1)
-        st.selectbox("Wild Card 3", nfc_teams, key="nfc_wild3", label_visibility="collapsed")
-        st.selectbox("Wild Card 4", nfc_teams, key="nfc_wild4", label_visibility="collapsed")
+        st.selectbox("Wild Card 3", nfc_teams, key="nfc_wild3", label_visibility="collapsed", disabled=dropdowns_disabled)
+        st.selectbox("Wild Card 4", nfc_teams, key="nfc_wild4", label_visibility="collapsed", disabled=dropdowns_disabled)
         vspace(1)
-        st.selectbox("Wild Card 5", nfc_teams, key="nfc_wild5", label_visibility="collapsed")
-        st.selectbox("Wild Card 6", nfc_teams, key="nfc_wild6", label_visibility="collapsed")
+        st.selectbox("Wild Card 5", nfc_teams, key="nfc_wild5", label_visibility="collapsed", disabled=dropdowns_disabled)
+        st.selectbox("Wild Card 6", nfc_teams, key="nfc_wild6", label_visibility="collapsed", disabled=dropdowns_disabled)
 
     # ---- CONTROLS SECTION (wider, centered below) ----
-    control_cols = st.columns([2, 2.5, 2])
+    control_cols = st.columns([2.3, 2.4, 2.3])
     
     with control_cols[1]:
-        st.markdown("### Controls")
-        tabs = st.tabs(["AutoML", "Choose Team"])
-
-        with tabs[0]:
-            st.markdown("#### AutoML (placeholder)")
-            if st.button("Run AutoML"):
-                pass
-
-        with tabs[1]:
-            chosen = st.selectbox(
-                "Pick a team",
-                ["--", "AFC_Wild1", "AFC_Conf1", "NFC_Wild1"]
-            )
+        with st.container(border=True):
+            c1, c2, c3 = st.columns([1, 2, 1])
+            with c2:
+                st.markdown("### Controls")
+            
+            # Simple toggle between modes - centered with columns
+            r1, r2, r3 = st.columns([0.8, 2.4, 0.8])
+            with r2:
+                mode = st.radio(
+                    "Control Mode",
+                    ["AutoML", "Choose Team"],
+                    index=0 if st.session_state.control_mode == "AutoML" else 1,
+                    horizontal=True,
+                    label_visibility="collapsed"
+                )
+            
+            # Update session state and rerun if mode changed
+            if mode != st.session_state.control_mode:
+                st.session_state.control_mode = mode
+                st.rerun()
+            
+            st.markdown("---")
+            
+            if mode == "AutoML":
+                st.markdown("#### AutoML (placeholder)")
+                if st.button("Run AutoML"):
+                    pass
+            else:
+                chosen = st.selectbox(
+                    "Pick a team",
+                    ["--", "AFC_Wild1", "AFC_Conf1", "NFC_Wild1"]
+                )
 
 # ---- TRAINING state ----
 if st.session_state.flow == "training":
